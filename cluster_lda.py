5809cec26ff03c9aa8e037b77d2845a4c5f033ee
Tom Rein <tomjrein@gmail.com>
2019-03-14 19:17:48 -0400

    added other clustering scripts

---
+++ cluster_lda.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from reduce_dimensions import perform_lda, plot_results
+from data import read_data, standardize_data
+
+def main():
+    names, stats, t1, t2, t3, t4 = read_data()
+    raw_stats = stats
+    stats = standardize_data(stats)
+
+    results = perform_lda(stats, t1)
+    plot_results(results, t1)
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
